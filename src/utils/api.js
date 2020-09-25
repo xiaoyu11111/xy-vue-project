16370684a5e4f2ba4d692ab72dfb1514bed9fd77
xiaoyu11111 <1848051540@qq.com>
2020-09-25 18:05:58 +0800

    feat:初始化项目

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+import {get,post} from './http.js'
+export default {
+  /* 
+   * 规范保存接口地址：
+   * xxxx: params => get/post('接口地址'，params), //这里请注释接口作用
+   * 
+   * 组件或页面调用时：
+   * this.$api.xxxx(params).then().catch();
+   */
+}
